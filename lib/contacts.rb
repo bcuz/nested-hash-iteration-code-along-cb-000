a467a3d5def2ae288d269f4c06fc93807cc29a86
adam n <adam.nawroc@gmail.com>
2018-03-02 14:32:27 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -28,7 +28,9 @@ def remove_strawberry(contacts)
     if attribute == :favorite_ice_cream_flavors
       value.each do |flavor|
         # here, each index element in an ice cream flavor string
-        puts "#{flavor}"
+        # puts "#{flavor}"
+
+        
       end
     end
   end
